49a20da80dfc5e92e8e7830543c02b278cfbcbb0
Olga Penkova <olya_kis_86@mail.ru>
2016-03-21 14:38:46 +0200

    structure change

---
+++ js/task3-4.js
@@ -0,0 +1,14 @@
+/*var ul = document.createElement('ul');
+ul.classlistAdd = 'menu';*/
+var ul = {
+	firstQuestion : function() {
+	var ulLiFirst = document.createElement('ulLiFirst');	
+    var ulLiFirst.innerHTML = 'Вопрос №1';
+	}
+	secondQuestion : function() {
+
+	}
+	thirdQuestion : function() {
+
+	}
+}
