0766f3f1a51fffa0b584402412ed4d69788f084f
Olga Penkova <olya_kis_86@mail.ru>
2016-04-14 14:37:50 +0300

    11-12 dz added not working

--- homework11-12js/js/task11-12-1.js
+++ homework11-12js/js/task11-12-1.js
@@ -1,12 +1,33 @@
 $(function() {
 
 var task = $('#task').html();
-var taskData = {
-  foto: 'https://scontent-waw1-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/168664_120009411402782_6391842_n.jpg?oh=748f3cc4d100f9f03dc7d691ad215840&oe=577083A2'
-};
-var content = tmpl(task, taskData);
+var taskData = [
+{
+    title: 'Пенькова Ольга Юрьевна',
+    content: ['https://scontent-waw1-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/168664_120009411402782_6391842_n.jpg?oh=748f3cc4d100f9f03dc7d691ad215840&oe=577083A2']
+},
+{
+	title: 'Финансист, на данный момент не работаю',
+	content: ['']
+},
+{
+    title: 'Хочу учить фронт-енд, потому что: ',
+    content: ['В сфере финансов не могу найти работу', 'Хочу работать в IT', 'Мне нравится фронт-енд']
+},
+{
+	title: 'Мои контакты: ',
+	content: ['тел.: +380997242342', 'skype: olya_kis1', '<a href="https://www.facebook.com/profile.php?id=100001811726892">facebook</a>']
+},
+{
+	title: 'Хобби: ',
+	content: ['Походы', 'Сплавы', 'Отдых с палаткой у костра']
+}
+];
+var page = tmpl(task, {
+	data: taskData
+});
 
-$('body').append(content);
+$('body').append(page);
 
 })
 
