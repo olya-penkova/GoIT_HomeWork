b6584bbc7033f0257fa9cf8450467b3e2eeeb257
Olga Penkova <olya_kis_86@mail.ru>
2016-04-21 16:11:28 +0300

    dz 15-16 added

--- homework15-16js/js/task15-16.js
+++ homework15-16js/js/task15-16.js
@@ -1,3 +1,32 @@
+//поисковая форма
+
+function GoogleCallback(func, data){
+	window[func](data);
+};
+var section;
+$(function() {
+$('form').on('submit', function(){
+    var url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&rsz=large&q="+ encodeURIComponent($('.search').val()) + "&callback=GoogleCallback&context=?";
+    $.ajax({
+    	url: url,
+    	dataType: 'jsonp',
+    	success: function(data, textStatus) {
+    		var ul = document.createElement('ul');
+    		$.each(data.results, function(i, val) {
+    			var li = document.createElement('li');
+    			li.innerHTML = '<a href="'+val.url+'" title="'+val.url+'" target="_blank">'+val.title+"</a> - "+val.content;
+    			ul.appendChild(li);
+    		});
+    		$('div').html(ul);
+    	}
+    });
+    return false;
+});
+
+});
+
+
+//прототипы
 function Human() {
 	this.name = 'Jack';
 	this.age = 30;
@@ -6,11 +35,14 @@ function Human() {
 	this.weight = 80;
 };
 
+Worker.prototype = new Human();
+Student.prototype = new Human();
+
 function Worker() {
 	this.job = 'farmer';
 	this.salary = 1000;
 	this.work = function() {
-		alert('wake up!');
+		alert('Это метод work!wake up!');
 	};
 };
 
@@ -18,12 +50,9 @@ function Student() {
 	this.university = 'DNTU';
 	this.grants = 500;
 	this.hobby = function() {
-		alert('turn on TV');
+		alert('Это метод hobby: whatch TV');
 	};
 };
-
-Worker.prototype = new Human();
-Student.prototype = new Human();
 
 var worker1 = new Worker();
 var worker2 = new Worker();
@@ -36,9 +65,6 @@ var student3 = new Student();
 console.log(worker1, worker2, worker3);
 console.log(student1, student2, student3);
 
-worker2.work();
-student3.hobby();
-
 console.log(worker1.name, worker1.age);
 console.log(worker2.height, worker2.weight);
 console.log(worker3.sex, worker3.name);
@@ -46,5 +72,7 @@ console.log(worker3.sex, worker3.name);
 console.log(student1.name, student1.university);
 console.log(student2.age, student2.age);
 console.log(student3.grants, student3.sex);
-
-
+$(function(){
+worker2.work();
+student3.hobby();
+})
