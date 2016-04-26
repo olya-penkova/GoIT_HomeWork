725b1d9bfa40403705e1ddc75a4546602bbca5b9
Olga Penkova <olya_kis_86@mail.ru>
2016-04-26 16:17:41 +0300

    dz 17-18 css.min added

--- homework9-10js/gruntfile.js
+++ homework9-10js/gruntfile.js
@@ -2,15 +2,22 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
    concat: {
-  options: {
-    // define a string to put between each file in the concatenated output
-    separator: ';'
-  },
-  dist: {
+  js: {
     // the files to concatenate
     src: ['js/src/*.js'],
     // the location of the resulting JS file
-    dest: 'js/dist/script.min.js'
+    dest: 'js/dist/script.min.js',
+    options: {
+    // define a string to put between each file in the concatenated output
+    separator: ';'
+  }
+  },
+  	css: {
+  	src: ['css/src/*.css'],
+  	dest: 'css/dist/style.min.css',
+  	options: {
+                separator: '\n\r'
+                }
   }
 },
 uglify: {
@@ -18,11 +25,23 @@ uglify: {
     src: ['js/dist/script.min.js'],
     dest: 'js/dist/script.min.js'
   }
+  },
+  cssmin: {
+  	target: {
+  	files: [{
+     expand: true,
+     cwd: 'css',
+  	 src: ['dist/style.min.css'],
+  	 dest: 'css',
+  	 ext: '.min.css'
+  }]
+}
 }
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-  grunt.registerTask('default', ['concat', 'uglify']);
+  grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
 };
