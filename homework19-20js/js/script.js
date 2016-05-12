fab8b1d0ec9c12e0b687d3472963cc11b3eb7f0a
Olga Penkova <olya_kis_86@mail.ru>
2016-05-12 16:58:50 +0300

    carousel change

--- homework19-20js/js/script.js
+++ homework19-20js/js/script.js
@@ -1,17 +1,23 @@
 $(function () {
-	$('.jcarousel').jcarousel();
+	/*$('.jcarousel').jcarousel();
 
 	$('.jcarousel-pagination').jcarouselPagination({
         item: function(page) {
             return '<a href="#' + page + '">' + '<div class="round">' + page+ '</div>' + '</a>';
     }
     });
+
     $(".round:first").addClass("active");
     $('.round').on('click', function() {
     	    $(this).addClass('active');
     	    $('.round').not($(this)).removeClass('active');
   
-        });
+        });*/
+
+    $('.flexslider').flexslider({
+    animation: "slide",
+    controlsContainer: $(".custom-controls-container"),
+  });
   
    function close_accordion_section() {
         $('.accordion .accordion-section-title').removeClass('active');
