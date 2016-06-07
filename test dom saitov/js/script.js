04e8b095fbec338468793b088c444123eca216bd
Olga Penkova <olya_kis_86@mail.ru>
2016-06-07 20:31:05 +0300

    test don saitov added

---
+++ test dom saitov/js/script.js
@@ -0,0 +1,10 @@
+$(function() {
+  $('.flexslider').flexslider({
+    animation: "slide",
+    animationLoop: false,
+    itemWidth: 375,
+    //itemMargin: 5,
+    minItems: 1,
+    maxItems: 3
+  });
+});
